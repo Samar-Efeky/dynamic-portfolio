8b532eab97717463c58239ddb5ded6ef713674d8
Samar-Elfeky <samarelfeky69@gmail.com>
2025-11-18 18:10:58 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,5 @@
 import { Routes } from '@angular/router';
-import { AdminPortfolio } from './admin-portfolio/admin-portfolio';
-import { Portfolio } from './portfolio/portfolio';
-export const routes: Routes = [
-    {
-        path:'portfolio-dashboard',
-        component:AdminPortfolio,
-        children:[
 
-        ]
-    },
-     {
-    path: 'portfolio',
-    component:Portfolio
-  },{
-        path:'', redirectTo:'portfolio',
-        pathMatch:'full'
-    }
+export const routes: Routes = [
+  
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Portfolio } from "./portfolio/portfolio";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ Portfolio],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('dynamic-portfolio');
 }
