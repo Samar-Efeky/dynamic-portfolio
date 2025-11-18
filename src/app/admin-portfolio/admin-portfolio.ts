e4f867b91cb8ad42bc7af4606a7b164f6d5eb9c9
Samar-Elfeky <samarelfeky69@gmail.com>
2025-11-18 17:43:54 +0200

    create project

--- src/app/admin-portfolio/admin-portfolio.ts
+++ src/app/admin-portfolio/admin-portfolio.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink } from "@angular/router";
-
 @Component({
   selector: 'app-admin-portfolio',
   imports: [],
@@ -8,8 +6,5 @@ import { Router, RouterLink } from "@angular/router";
   styleUrl: './admin-portfolio.scss',
 })
 export class AdminPortfolio {
-  // constructor(private router:Router){}
-  //  goToPortfolio(username:string) {
-  //   this.router.navigate(['/portfolio', username]);
-  // }
+ 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,7 +13,7 @@ export const routes: Routes = [
     path: 'portfolio',
     component:Portfolio
   },{
-        path:'',redirectTo:'portfolio',
+        path:'', redirectTo:'portfolio',
         pathMatch:'full'
     }
 ];
