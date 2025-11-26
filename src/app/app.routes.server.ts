eb5c25438330e7e155485e0147ea5abfcd985936
Samar-Elfeky <samarelfeky69@gmail.com>
2025-11-26 18:27:13 +0200

    update

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,17 +1,6 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  
-  {
-    path: 'portfolio/:username',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'portfolio/:username/**',
-    renderMode: RenderMode.Server
-  },
-
-  // باقي الموقع prerender
   {
     path: '**',
     renderMode: RenderMode.Prerender
